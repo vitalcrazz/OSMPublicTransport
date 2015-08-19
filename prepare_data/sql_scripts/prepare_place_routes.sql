68b25261f8121cc525a1d692f959abd359fd6019
vitalcrazz <pjil@yandex.ru>
2015-08-19 17:49:59 +0400

    a little bit of generalization

--- prepare_data/sql_scripts/prepare_place_routes.sql
+++ prepare_data/sql_scripts/prepare_place_routes.sql
@@ -57,3 +57,35 @@ FROM
 		GROUP BY place_id, route_type, refnum, way_id, route_id, way_pos, oneway) as ways
 	GROUP BY place_id, route_type, way_id, geom, oneway) as linked_ways
 GROUP BY place_id, route_type, routes, oneway;
+
+-- need to delete geom2 from p2; geom1 stay as is
+-- need to add to p1 all routes of p2
+-- need to
+UPDATE place_routes AS pr
+SET geom = pgeom
+FROM (
+	SELECT ST_Difference(geom, geom2) AS pgeom, pid
+	FROM (
+		SELECT p2.id AS pid, 
+			p2.geom AS geom,
+			(ST_Dump((p1.geom))).path AS path1, 
+			(ST_Dump(p1.geom)).geom AS geom1, 
+			(ST_Dump((p2.geom))).path AS path2, 
+			(ST_Dump(p2.geom)).geom AS geom2
+		FROM place_routes AS p1, place_routes AS p2
+		WHERE p1.oneway = TRUE AND
+			p2.oneway = TRUE AND
+			p1.id > p2.id AND
+			p1.place_id = p2.place_id AND
+			p1.routes && p2.routes AND
+			ST_Intersects(p1.geom, p2.geom) AND
+			ST_NumGeometries(ST_Intersection(p1.geom, p2.geom)) > ST_NumGeometries(p1.geom) and
+			ST_NumGeometries(ST_Intersection(p1.geom, p2.geom)) > ST_NumGeometries(p2.geom)
+	) AS T1
+	WHERE ST_Intersects(geom1, geom2) and ST_NumGeometries(ST_Intersection(geom1, geom2)) > 1
+) AS T2
+WHERE pr.id = T2.pid;
+
+SELECT ST_AsGeoJson(geom)
+FROM place_routes
+WHERE ST_IsClosed(geom);
