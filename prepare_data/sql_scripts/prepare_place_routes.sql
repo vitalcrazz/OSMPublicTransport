89898cd5eb6d03c8f83cd8238fa7c370105a8ed2
vitalcrazz <pjil@yandex.ru>
2015-08-19 13:21:38 +0400

    try to handle service and oneway roads

--- prepare_data/sql_scripts/prepare_place_routes.sql
+++ prepare_data/sql_scripts/prepare_place_routes.sql
@@ -4,21 +4,24 @@ place_routes (
 	place_id INT NOT NULL,
 	route_type VARCHAR(128) NOT NULL,
 	routes TEXT[] NOT NULL,
+	oneway BOOLEAN NOT NULL,
 	geom GEOMETRY NOT NULL);
 
 TRUNCATE TABLE place_routes;
 
-INSERT INTO place_routes (place_id, route_type, routes, geom)
+INSERT INTO place_routes (place_id, route_type, routes, oneway, geom)
 SELECT
 	place_id,
 	route_type,
 	routes,
+	oneway,
 	ST_LineMerge(ST_Union(linked_ways.geom)) as geom
 FROM
 	(SELECT
 		place_id,
 		route_type,
 		way_id,
+		oneway,
 		array_agg(DISTINCT refnum) AS routes,
 		geom
 	FROM
@@ -27,12 +30,14 @@ FROM
 			route_type,
 			refnum,
 			way_id,
+			oneway,
 			ST_makeLine(nodes.geom) as geom
 		FROM
 			(SELECT
 					relations.id as route_id,
 					relations.tags->'ref' as refnum,
 					relations.tags->'route' as route_type,
+					(exist(ways.tags, 'oneway') and ways.tags->'oneway'!='no') as oneway,
 					t_nodes.node_pos as node_pos,
 					relation_members.sequence_id as way_pos,
 					relation_members.member_id as way_id,
@@ -45,9 +50,10 @@ FROM
 					relations.id=relation_members.relation_id and
 					relation_members.member_role in('','forward','backward') and
 					relation_members.member_id=ways.id and
-					nodes.id = t_nodes.node_id
+					nodes.id = t_nodes.node_id and
+					ways.tags->'highway'!='service'
 				ORDER BY way_pos, node_pos) as nodes
 			JOIN transport_location USING(route_id)
-		GROUP BY place_id, route_type, refnum, way_id, route_id, way_pos) as ways
-	GROUP BY place_id, route_type, way_id, geom) as linked_ways
-GROUP BY place_id, route_type, routes;
+		GROUP BY place_id, route_type, refnum, way_id, route_id, way_pos, oneway) as ways
+	GROUP BY place_id, route_type, way_id, geom, oneway) as linked_ways
+GROUP BY place_id, route_type, routes, oneway;
